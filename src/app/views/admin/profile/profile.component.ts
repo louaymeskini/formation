7ac88bf44311f1c73d673c702851ece03c030510
Louay MESKINI <louay.meskini@proxym-it.com>
2019-12-06 16:11:48 +0100

    guard fix

--- src/app/views/admin/profile/profile.component.ts
+++ src/app/views/admin/profile/profile.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {AssociationService} from '../../services/association.service';
-import {AdminService} from '../../services/admin.service';
-import {AdminModel} from '../../models/admin.model';
-import {normalizeSourceMaps} from '@angular-devkit/build-angular/src/utils';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -12,71 +6,10 @@ import {Observable} from 'rxjs';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  // adminForm: FormGroup;
-  submitted: boolean;
-  adminInfo: AdminModel [] = [];
 
-  adminForm: FormGroup = new FormGroup({
-    nom: new FormControl(null,
-      [Validators.minLength(4)]),
-    prenom: new FormControl(null,
-      [Validators.minLength(4)]),
-    email: new FormControl(null,
-      [Validators.email]),
-    username: new FormControl(null,
-      [Validators.minLength(4)]),
-    password: new FormControl(null,
-      [Validators.required, Validators.minLength(4)])
-  });
-
-  constructor(private adminService: AdminService) {
-    this.validate();
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.submitted = false;
-    this.previousValue().subscribe(res => {
-      console.log('adminInfo3', this.adminInfo);
-    });
-  }
-
-  validate () {
-    this.adminForm = new FormGroup({
-        nom: new FormControl(null,
-          [Validators.minLength(4)]),
-        prenom: new FormControl(null,
-          [Validators.minLength(4)]),
-        email: new FormControl(null,
-          [Validators.email]),
-        username: new FormControl(null,
-          [Validators.minLength(4)]),
-        password: new FormControl(null,
-          [Validators.required, Validators.minLength(4)])
-    });
-  }
-
-  previousValue () {
-    // tslint:disable-next-line:prefer-const
-    // let info: AdminModel [] = [];
-    return new Observable(observer => {
-      this.adminService.fetchAdmin().subscribe(res => {
-        // this.adminInfo = res;
-        this.adminInfo.push(res);
-        observer.next(true);
-        // this.adminInfo = [].concat.apply([], info);
-        console.log('adminInfo1', this.adminInfo[0]);
-      });
-    });
-  }
-
-  onSubmit () {
-    this.submitted = true;
-    console.log('form', this.adminForm);
-    if (this.adminForm.valid) {
-      console.log('valid');
-    } else {
-      console.log('invalid');
-    }
   }
 
 }
