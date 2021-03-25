47530fb8fb22e738b119fb8d2c4e6927636ca1bc
louay.meskini <louay.meskini@proxym-it.com>
2021-03-25 14:26:22 +0100

    [heroku] server + .

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/pfa'));
+app.use(express.static(__dirname + '/dist/pfa'));
 
 app.get('/*', function(req,res) {
 
